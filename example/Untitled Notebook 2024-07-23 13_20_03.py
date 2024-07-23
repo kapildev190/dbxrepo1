732a0882bbf4f54836fa3328f10281e7c00040eb
kapildev190 <kapildev190>
2024-07-23 08:37:34 +0000

    First commit from local.
    Adding two notebooks in example folder

---
+++ example/Untitled Notebook 2024-07-23 13_20_03.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+display("Hello, First notebook of Databricks.")
+
+# COMMAND ----------
+
+print("Hello from python")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select "Hello form SQL"
